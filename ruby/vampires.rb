c14a03d4241ba552ba3873cde1c802a2c01b595a
alexnhill <alexandrahillolds@gmail.com>
2017-03-09 11:32:48 -0500

    Create interview form with questions and requested inputs

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+
+puts "What is your name?"
+vamp_name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? y/n"
+health_insurance = gets.chomp
+
