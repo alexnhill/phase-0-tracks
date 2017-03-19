8caadb81bfc124acfd0da0c6ce50bc8522155e24
alexnhill <alexandrahillolds@gmail.com>
2017-03-19 00:35:49 -0400

    Fix name inputs so that only one is being passed it. Alter program to accomodate this.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,56 +11,56 @@
 def letter_change(name)
   vowels = ["a", "e", "i", "o", "u"]
   consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' ]
-  name = name.downcase.chars
-  name_new = name.map! do |letter|
-    if vowels.include?(letter)
-      vowels.rotate[vowels.index(letter)]
-    elsif consonants.include?(letter)
-      consonants.rotate[consonants.index(letter)]
-    elsif letter[index] == "z"
-      letter[index] == "b"
-    elsif letter[index] == "u"
-      letter[index] == "a"  
+  name = name.downcase.chars #takes the input and makes it downcase and breaks it into an array to increment through
+  name_new = name.map do |letter| #create a new array called name_new and start block to increment through each array
+    if vowels.include?(letter) #conditional to check if the letter is in the vowels array above
+      vowels.rotate[vowels.index(letter)] #within the vowels array.rotate over by one[the index of the letter in the vowels array]
+    elsif consonants.include?(letter) #conditional to check if the letter is in the consonant array defined above
+      consonants.rotate[consonants.index(letter)] #because this has met the above condition, we know that the char exists in the array. we are asking for the index of the letter in the consonants array and from there are rotate to the next by one. This movement is being saved into the new name_new array.
+    elsif letter == "z"
+      letter == "b"
+    elsif letter == "u"
+      letter == "a"  
+     elsif letter == " "
+      letter = " " 
     end
   end
-  name_new.join('').capitalize
+  #Code addressing capitalization and swapping first / last
+  namearr = name_new.join('') #join the new_name array back into a string
+  namearr2 = namearr.split #re-seperate it into two index items
+  first_name = namearr2[0] #assign first name to a new variable based on index
+  last_name = namearr2[1] ##assign first name to a new variable based on index
+  name_new = last_name.capitalize! + " " + first_name.capitalize! #capitalize and swap first for last
 end
 
 #method that swaps first and last names
 
-def name_swap(first, last)
-  new_first = ''
-  new_last = ''
-		new_first << letter_change(first)
-		new_last << letter_change(last)
-	puts (new_last + ' ' + new_first)	
+def name_swap(real_name)
+	new_name = letter_change(real_name)
+  p new_name
+  return new_name
 end	
+
 
 #hash that stores first/last old and new_first
 
 code_names = {
-  first_name: [],
-  last_name: [],
-  new_first: [],
-  new_last: []
+  real_name: [],
+  fake_name: []
 }
+
 
 #takes names (UI)
 
 loop do
-puts "enter your first name or type 'quit' when you're done!"
-first_name = gets.chomp.to_s
-code_names[:first_name].push(first_name)
-break if first_name == 'quit' 
+puts "Enter your first and last name or type 'quit' when you're done!"
+real_name = gets.chomp
+code_names[:real_name].push(real_name)
+if real_name == 'quit' 
+  p code_names
+  break
+ end 
 
-puts "enter your last name or type 'quit' when you're done!"
-last_name = gets.chomp.to_s
-code_names[:last_name].push(last_name)
-break if last_name == 'quit'
-
-name_swap(first_name, last_name)
-
-#print code_names hash to test
-p code_names
+code_names[:fake_name].push(name_swap(real_name))
 
 end
