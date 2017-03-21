461f6a06c5e590b099329cb48a9e603a87d796ac
alexnhill <alexandrahillolds@gmail.com>
2017-03-21 12:27:13 -0400

    Add puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+
+# Release :: Design a Class
+
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Name: varies
+# Size: varies
+# Hair length: varies
+# Tail: varies (long/short)
+# Friendly: yes
+# Trainable: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Play
+# Lick
+# Sleep
