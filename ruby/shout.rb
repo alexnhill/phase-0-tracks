860bb83a39e5776ee08fdcf13b1f34f57ef44841
alexnhill <alexandrahillolds@gmail.com>
2017-03-23 10:32:25 -0400

    Modify module to accomodate classes and create new classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,43 @@
 
+# RELEASE 1
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end	
+# 	def self.yell_happily(words)
+# 		words + "yiipeee!" + " :)"
+# 	end	
+# end
+
+# RELEASE 3
 
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end	
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "yiipeee!" + " :)"
 	end	
 end
 
-# DRIVER CODE
-#p Shout.yell_happily(Shout.yell_angrily("Hi!"))
+
+class Angry_dad
+	include Shout
+end	
+
+class Cheerleader
+	include Shout
+end	
+
+# RELEASE 1 DRIVER CODE
+# p Shout.yell_happily(Shout.yell_angrily("Hi!"))
 # p Shout.yell_angrily("hi")
 # p Shout.yell_happily("hi")
+
+# RELEASE 3 DRIVER CODE
+
+dad = Angry_dad.new
+p dad.yell_angrily("no")
+
+sally = Cheerleader.new
+p sally.yell_happily("yay")
