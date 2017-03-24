84f4c7f666e6e3563c219b675ced41a62170dbcb
alexnhill <alexandrahillolds@gmail.com>
2017-03-24 16:45:17 -0400

    Tweak spec and program file to add features and tweak existing code

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,51 +1,75 @@
 # WORD GAME
 
 class WordGame
-attr_accessor :secret_word, :placeholder_word, :game_over, :guess_counter, :previous_guesses
+attr_accessor :secret_word, :placeholder_word, :game_over, :guess_counter, :previous_guesses, :num_guesses
+attr_reader
+attr_writer
 
 	def initialize(word)
 		@secret_word = word.chars
-		@guess_counter = 0
+		@guess_counter = 1
 		@game_over = false
 		@placeholder_word = ("-" * (secret_word.length.to_i)).chars
+		@num_guesses = secret_word.length * 2
 		@previous_guesses = []
 	end
 
 	def count_guesses(char)
-		num_guesses = secret_word.length * 2
+		p guess_counter
 		if previous_guesses.include?(char)
-			current_guess = num_guesses - guess_counter
-		else	
-			guess_counter =+ 1
+			current_guess = 1 + (num_guesses - guess_counter)
+		else
 			current_guess = num_guesses - guess_counter
 		end
-		puts " You have #{current_guess} guesses. Enter a letter to guess."
+		puts "You have #{current_guess} guesses. Enter a letter to guess."
   	end
 
 
 	def guess_checker(char)
-		if secret_word.include?(char)
-			placeholder_word[secret_word.index(char)] = char
-			print placeholder_word
-		end	
 		count_guesses(char)
-		previous_guesses << char
+		  if secret_word.include?(char)
+		  	placeholder_word[secret_word.index(char)] = char
+		  	puts placeholder_word.join
+		  else
+		  	puts placeholder_word.join
+		  end
+		   guess_counter += 1
+		  # placeholder_word = secret_word.each do |a| 
+		 	# if secret_word.include?(char)
+		  #  		placeholder_word[secret_word.index(char)] = char
+		  #  	else 
+		  #  		a = "-"
+		  #  	end
+		  #  	placeholder_word.join
+		  #  	puts placeholder_word
+	   # 	   end
 	end
 
 end
 
 
-#DRIVER CODE
+# UI 
 new_word = WordGame.new("peaches")
 
-new_word.guess_checker("a")
+ while !new_word.game_over
+   puts "Guess a letter"
+   letter = gets.chomp
+   new_word.guess_checker(letter)
+   new_word.previous_guesses << letter
+   print new_word.previous_guesses #to show array filling in
+   # if !new_word.placeholder_word == new_word.secret_word
+   #   	puts "You won!"
+   #   	game_over = true
+   #  elsif new_word.guess_counter < new_word.num_guesses
+  	# 	game_over = false
+  	# else letter = 'exit'	
+  	# 	game_over = true
+  end
 
-# while !new_word.game_over
- # 	if placeholder_word == secret_word
- # 		game_over = true
- #  elsif num_guesses == guess_counter
- # 		game_over = true
- # 	else guess_counter < num_guesses 
- # 		 "try again"
- # 		game_over = false
- # end
+#DRIVER CODE
+
+# new_word.num_guesses.times do
+new_word.guess_checker("e")
+# end
+
+
--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -1,15 +1,20 @@
 require_relative 'word_game'
 
 describe WordGame do
-	let(:secret_word) { WordGame.new("peaches") }
+	let(:new_word) { WordGame.new("peaches") }
 
  it "stores the secret word given as an array upon initialization" do
-    expect(secret_word.word_print).to eq ["p", "e", "a", "c", "h", "e", "s"]
+    expect(new_word.secret_word).to eq ["p", "e", "a", "c", "h", "e", "s"]
   end
 
   it "generates a placeholder string for user output" do
-  	expect(placeholder_word.word_print).to eq ["-", "-", "-", "-", "-", "-", "-"]
+  	expect(new_word.placeholder_word).to eq ["-", "-", "-", "-", "-", "-", "-"]
   end
+
+  it "guesses what the secret word is and generates a placeholder response" do
+  	expect(new_word.guess_checker("p")). to eq "p------"
+  end
+
 
  # it "checks to see if letter guessed is part of secret word" do 
  # 	expect(secret_word.guess) to eq "- - - - - - -"
