d1bcbc06d9de4fd82c6af26794d88db167819270
alexnhill <alexandrahillolds@gmail.com>
2017-03-29 18:55:10 -0400

    Refactor the methods to be individualized and more compact

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,22 +8,11 @@
 	#turn it back into a string and capitalize
 
 
-def letter_change(name)
-  vowels = ["a", "e", "i", "o", "u"]
-  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' ]
+#method that handles the breakdown and rebuild of the names, once the letters have been changed in the letter_change method
+def name_handler(name)
   name = name.downcase.chars #takes the input and makes it downcase and breaks it into an array to increment through
   name_new = name.map do |letter| #create a new array called name_new and start block to increment through each array
-    if vowels.include?(letter) #conditional to check if the letter is in the vowels array above
-      vowels.rotate[vowels.index(letter)] #within the vowels array.rotate over by one[the index of the letter in the vowels array]
-    elsif consonants.include?(letter) #conditional to check if the letter is in the consonant array defined above
-      consonants.rotate[consonants.index(letter)] #because this has met the above condition, we know that the char exists in the array. we are asking for the index of the letter in the consonants array and from there are rotate to the next by one. This movement is being saved into the new name_new array.
-    elsif letter == "z"  #edge case
-      letter == "b"
-    elsif letter == "u" #edge case
-      letter == "a"  
-     elsif letter == " " #edge case
-      letter = " " 
-    end
+    letter_change(letter)
   end
   #Code addressing capitalization and swapping first / last
   namearr = name_new.join('') #join the new_name array back into a string
@@ -33,11 +22,28 @@ def letter_change(name)
   name_new = last_name.capitalize! + " " + first_name.capitalize! #capitalize and swap first for last
 end
 
-#method that swaps first and last names
+#helper method to change names
+def letter_change(letter)
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' ]
+ if vowels.include?(letter) #conditional to check if the letter is in the vowels array above
+      vowels.rotate[vowels.index(letter)] #within the vowels array.rotate over by one[the index of the letter in the vowels array]
+  elsif consonants.include?(letter) #conditional to check if the letter is in the consonant array defined above
+      consonants.rotate[consonants.index(letter)] #because this has met the above condition, we know that the char exists in the array. we are asking for the index of the letter in the consonants array and from there are rotate to the next by one. This movement is being saved into the new name_new array.
+  elsif letter == "z"  #edge case
+      letter == "b"
+  elsif letter == "u" #edge case
+      letter == "a"  
+  elsif letter == " " #edge case
+      letter = " " 
+  end
+end
+
+#method that takes in the values and then prints & returns them
 
 def name_swap(real_name)
-	new_name = letter_change(real_name)
-  p new_name
+	new_name = name_handler(real_name)
+  puts new_name
   return new_name
 end	
 
