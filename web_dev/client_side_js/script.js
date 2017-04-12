f0608aacf6375923804a874482f0539ca1d603f9
alexnhill <alexandrahillolds@gmail.com>
2017-04-12 15:17:25 -0400

    Add an event listener for a final dramatic effect

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,10 @@
 
 
+function clickRed (event) {
+	console.log (event);
+	event.target.style.backgroundColor = "red";
+}
+
 var bul1 = document.getElementById("el1");
 bul1.style.border = "2px solid pink";
 
@@ -8,3 +13,5 @@ bul2.style.backgroundColor = "blue";
 
 var bul3 = document.getElementById("el3");
 bul3.style.fontSize = "50px";
+bul3.addEventListener("click", clickRed);
+
