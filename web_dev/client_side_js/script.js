7f7af61770ba7827997e5a03db1dd1d9b32f43a1
alexnhill <alexandrahillolds@gmail.com>
2017-04-12 15:10:41 -0400

    Create index and script files and added basic javascript to style elements

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,10 @@
+
+
+var bul1 = document.getElementById("el1");
+bul1.style.border = "2px solid pink";
+
+var bul2 = document.getElementById("el2");
+bul2.style.backgroundColor = "blue";
+
+var bul3 = document.getElementById("el3");
+bul3.style.fontSize = "50px";
